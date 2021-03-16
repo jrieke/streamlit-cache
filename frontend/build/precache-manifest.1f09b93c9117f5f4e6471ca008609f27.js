2e7ca82296cd62636e71ee432a87e03dbd0cdb7e
Johannes Rieke <johannes.rieke@gmail.com>
2021-03-16 19:23:03 +0100

    Add build dir for component

---
+++ frontend/build/precache-manifest.1f09b93c9117f5f4e6471ca008609f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82287a45778528a5b94802cc97172a90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aca78e865a6084af9044",
+    "url": "./static/js/2.df3862f6.chunk.js"
+  },
+  {
+    "revision": "8ffea70bc0c3f181bbb22cd4b9761122",
+    "url": "./static/js/2.df3862f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327ffeeb7549717a2ab3",
+    "url": "./static/js/main.a2239661.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
