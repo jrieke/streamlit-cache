ed2625e1562b74312b727486d014f6a6e562d2d5
Johannes Rieke <johannes.rieke@gmail.com>
2021-03-16 02:01:21 +0100

    Update filenames

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./MyComponent"
+import UserIdComponent from "./UserIdComponent"
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent />
+    <UserIdComponent />
   </React.StrictMode>,
   document.getElementById("root")
 )
